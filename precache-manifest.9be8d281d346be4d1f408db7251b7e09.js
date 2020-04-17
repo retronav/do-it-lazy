738fbc37477788976363b12124ca42928106763b
Pranav Karawale <pdkarawale31@gmail.com>
2020-04-17 12:57:04 +0530

    Updates

---
+++ precache-manifest.9be8d281d346be4d1f408db7251b7e09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9dbb22f8d1968990215ed64c9db438f",
+    "url": "/do-it-lazy/index.html"
+  },
+  {
+    "revision": "6de4c842212c9e4596d6",
+    "url": "/do-it-lazy/static/css/main.cb9dec1b.chunk.css"
+  },
+  {
+    "revision": "c2a3495d8f5b56b87326",
+    "url": "/do-it-lazy/static/js/2.cfd5be32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/do-it-lazy/static/js/2.cfd5be32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6de4c842212c9e4596d6",
+    "url": "/do-it-lazy/static/js/main.58b90839.chunk.js"
+  },
+  {
+    "revision": "da39210e09bdc3c28bd5",
+    "url": "/do-it-lazy/static/js/runtime-main.d77f7f66.js"
+  },
+  {
+    "revision": "5678656dbbbd061351139897ef4d60bb",
+    "url": "/do-it-lazy/static/media/header.5678656d.jpg"
+  }
+]);
