ca0bc6236215efa039976f513f43ffca61b8535a
Pranav Karawale <pdkarawale31@gmail.com>
2020-04-19 20:04:16 +0530

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -14,27 +14,29 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register({
+const options = {
   onSuccess: () => {
     DefaultToast.fire("You can now use this app when offline.");
   },
-  onUpdate: (reg) => {
+  onUpdate: () => {
     UpdateToast.fire("A newer version is available. Update??").then((res) => {
       if (!res.dismiss) {
-        /*eslint-disable no-restricted-globals*/
-        self.addEventListener("install", () => {
-          self
-            //@ts-ignore
-            .skipWaiting()
-            .then(() => {
-              window.location.reload();
-            })
-            //@ts-ignore
-            .catch((res) => {
-              console.error(`Couldn't update! ${res}`);
+        navigator.serviceWorker
+          .getRegistrations()
+          .then((regs) => {
+            regs.forEach((reg) => {
+              reg.unregister();
             });
-        });
+          })
+          .then(() => {
+            window.location.reload();
+          })
+          .catch((res) => {
+            console.error(`Couldn't update! ${res}`);
+          });
+        serviceWorker.register(options);
       }
     });
   },
-});
+};
+serviceWorker.register();
